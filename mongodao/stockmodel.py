4ba7be58197a1d9529ba1cce5bf9458f1e782eac
liangjian <1738757@qq.com>
2017-07-28 20:20:21 +0800

    微信股票

--- mongodao/stockmodel.py
+++ mongodao/stockmodel.py
@@ -1,4 +1,3 @@
-
 from mongodao import mongoclient
 
 
@@ -12,6 +11,28 @@ class KStockModel(mongoclient.MClient):
         super().__init__('stock', 'k_stock')
 
 
+class K30StockModel(mongoclient.MClient):
+    def __init__(self):
+        super().__init__('stock', 'k30_stock')
 
 
+PERDAYTYPE = 'perdaytask'
 
+
+class SchedulerConfig(mongoclient.MClient):
+    def __init__(self):
+        super().__init__('stock', 'scheduler_config')
+
+    def setperdaydate(self, ktype, date):
+        self.update({
+            'start_date_' + ktype: date
+        }, {'type': PERDAYTYPE})
+
+    def get_scheduler_config(self):
+        return self.find_one(type=PERDAYTYPE)
+
+    def insert_fail_kstock_code(self, start_date, stock_list):
+        self.insert_one({'type': PERDAYTYPE,
+                         'start_date': start_date,
+                         'stock_list': stock_list,
+                         'is_repair': 'no'})
--- stock_task/perday_task.py
+++ stock_task/perday_task.py
@@ -7,37 +7,55 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 basic_model = stockmodel.BasicStockModel()
 k_model = stockmodel.KStockModel()
+sche_config = stockmodel.SchedulerConfig()
+k30_model = stockmodel.K30StockModel()
+
+DateFmt = '%Y-%m-%d'
 
 
 # 股票列表
 def basic_data():
     basic = ts.get_stock_basics()
-    # data = json.loads(basic_data.to_json(orient='records'))
     for i in basic.index:
         item = basic.ix[i].to_dict()
         item['code'] = i
         item['timeToMarket'] = float(item['timeToMarket'])
+
         item['holders'] = float(item['holders'])
         basic_model.replace_one(item, code=i)
 
 
 # 日k线
-def day_k_stock():
+def day_k_stock(cb, ktype='D', start_date='2017-01-01'):
     stock_list = basic_model.find()
-    now = datetime.date.today().strftime('%Y-%m-%d')
-    yestoday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+    config = sche_config.get_scheduler_config()
+    if config is not None and 'start_date' in config:
+        start_date = config['start_date']
+    fail_stock_list = []
     for stock in stock_list:
         print(stock['code'])
-        k = ts.get_k_data(stock['code'], start='2017-07-11', end=now)
-        if len(k) > 0:
-            data = json.loads(k.to_json(orient='records'))
-            k_model.insert(data)
+        try:
+            k = ts.get_k_data(stock['code'], start=start_date, ktype=ktype)
+            if len(k) > 0:
+                data = json.loads(k.to_json(orient='records'))
+                if cb is None:
+                    k_model.insert(data)
+                else:
+                    cb(data)
+        except Exception as e:
+            fail_stock_list.append(stock['code'])
+
+    start_date = datetime.datetime.strptime(start_date, DateFmt) + datetime.timedelta(days=1)
+    sche_config.setperdaydate(ktype, start_date.strftime(DateFmt))
+    if len(fail_stock_list) > 0:
+        sche_config.insert_fail_kstock_code(start_date, fail_stock_list)
 
 
 def per_day_task():
     print('start perday task')
     basic_data()
     day_k_stock()
+    day_k_stock(lambda data: k30_model.insert(data), 30, '2017-07-01')
 
 
 def done(e):
@@ -46,6 +64,6 @@ def done(e):
 
 if __name__ == '__main__':
     scheduler = BlockingScheduler()
-    scheduler.add_job(per_day_task, 'cron', hour=5, minute=50)
+    scheduler.add_job(per_day_task, 'cron', hour=19, minute=40)
     scheduler.add_listener(done, events.EVENT_JOB_EXECUTED)
     scheduler.start()
